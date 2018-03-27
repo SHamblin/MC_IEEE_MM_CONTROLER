5cc8ae39bc5a46ffca4f2f00538c277d09c1d297
Francesco Gallegos <alessandro0915@gmail.com>
2018-03-27 18:40:26 -0400

    New Branch

---
+++ Dijkstra's Algorithm.py
@@ -0,0 +1,44 @@
+# This algorithm can find the shortest distance in a graph
+    # We can pass in a graphed version of map
+
+def dijkstra(graph,start,goal):
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = graph
+    infinity = 99999999
+    path = []
+
+    for node in unseenNodes:
+        shortest_distance[node] = infinity
+    shortest_distance[start] = 0
+
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+
+        for childNode, weight in graph[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                predecessor[childNode] = minNode
+        unseenNodes.pop(minNode)
+
+    currentNode = goal
+
+    while currentNode != start:
+        try:
+            path.insert(0,currentNode)
+            currentNode = predecessor[currentNode]
+        except KeyError:
+            print("Ooops! Something went wrong")
+            break
+
+    path.insert(0, start)
+    if shortest_distance[goal] != infinity:
+        print('shortest_distance is ' + str(shortest_distance[goal]))
+        print(str(path))
+
+dijkstra(graph, 'a', 'e')
--- testresult.py
+++
@@ -1,5 +0,0 @@
-import findPath
-import testmatrices
-
-path = findPath.path(0, 0, 5, 5, testmatrices.visited, testmatrices.pathfound, testmatrices.matr)
-print(path)
